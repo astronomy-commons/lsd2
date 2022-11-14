7b806e6c6e6e7c314103732e3832a901e7020630
Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>
2022-11-14 15:33:18 -0500

    Pyproject and code organization

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="hipscat-config",
-    author="Sam Wyatt",
-    url="https://github.com/astronomy-commons/lsd2",
-    setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
-    packages=["hipscat"],
-    include_package_data=True
-)
+setup(setup_requires=['setuptools_scm'])
