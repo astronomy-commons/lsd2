ed4227a4c69ab5e13472c7d950574c3c1c6c7dbe
delucchi-cmu <delucchi@andrew.cmu.edu>
2022-12-05 09:05:02 -0500

    Expanding docstrings and cleaning list check

--- src/partitioner/histogram.py
+++ src/partitioner/histogram.py
@@ -7,23 +7,46 @@ from partitioner.io_utils import read_dataframe
 
 
 def empty_histogram(highest_order):
-    """Use numpy to create an histogram array with the right shape, filled with zeros"""
+    """Use numpy to create an histogram array with the right shape, filled with zeros.
+    Args:
+        highest_order (int): the highest healpix order (e.g. 0-10)
+    Returns:
+        one-dimensional numpy array of long integers, where the length is equal to
+        the number of pixels in a healpix map of target order, and all values are set to 0.
+    """
     return np.zeros(hp.order2npix(highest_order), dtype=np.ulonglong)
 
 
 def generate_partial_histogram(
     file_path,
-    highest_order,
     file_format,
+    highest_order,
     ra_column,
     dec_column,
 ):
-    """Generate a histogram of counts for objects found in the indicated file_path"""
+    """Generate a histogram of counts for objects found in the indicated file_path
+
+    Args:
+        file_path (str): full path to the input file
+        file_format (str): expected format for the input file. See io_utils.read_dataframe
+            for accepted formats.
+        highest_order (int):  the highest healpix order (e.g. 0-10)
+        ra_column (str): where in the input to find the celestial coordinate, right ascension
+        dec_column (str): where in the input to find the celestial coordinate, declination
+    Returns:
+        one-dimensional numpy array of long integers where the value at each index corresponds
+        to the number of objects found at the healpix pixel.
+    Raises:
+        ValueError: if the `ra_column` or `dec_column` cannot be found in the input file.
+        FileNotFoundError: See io_utils.read_dataframe for other error conditions.
+    """
     histo = empty_histogram(highest_order)
     data = read_dataframe(file_path, file_format)
 
+    required_columns = [ra_column, dec_column]
+
     # Verify that the file has columns with desired names.
-    if not all([x in data.columns for x in [ra_column, dec_column]]):
+    if not all(x in data.columns for x in required_columns):
         raise ValueError(
             f"Invalid column names in input file: {ra_column}, {dec_column} not in {file_path}"
         )
--- src/partitioner/io_utils.py
+++ src/partitioner/io_utils.py
@@ -7,7 +7,23 @@ from astropy.table import Table
 
 
 def read_dataframe(path="", file_format="parquet") -> pd.DataFrame:
-    """Read a file in as a dataframe"""
+    """Read a file in as a dataframe.
+
+    Currently supported file formats include:
+        - `csv` - comma separated values
+        - `parquet` - apache columnar data format
+        - `fits` - flexible image transport system
+
+    Args:
+        path (str): fully-specified path to the file
+        file_format (str): expected file format for the input file. This 
+            likely matches the extension of the file, but doesn't need to.
+    Returns:
+        dataframe with the data content at the target file
+    Raises:
+        FileNotFoundError: if there is no regular file at the indicated `path`.
+        NotImplementedError: if the file format is not yet supported.
+    """
 
     # Perform checks on the provided path
     if not os.path.exists(path):
@@ -21,12 +37,13 @@ def read_dataframe(path="", file_format="parquet") -> pd.DataFrame:
     # Load file using appropriate mechanism
     if "csv" in file_format:
         data_frame = pd.read_csv(path)
-    elif "parquet" in file_format:
+    elif file_format == "parquet":
         data_frame = pd.read_parquet(path, engine="pyarrow")
-    elif "fits" in file_format:
+    elif file_format == "fits":
         dat = Table.read(path, format="fits")
         data_frame = dat.to_pandas()
     else:
         raise NotImplementedError(f"File Format: {file_format} not supported")
+
 
     return data_frame
