14a7b01d92f42399c42b5f7882d570f3f552cea6
delucchi-cmu <delucchi@andrew.cmu.edu>
2022-12-07 09:29:34 -0500

    time-domain example

---
+++ examples/td_demo_new.py
@@ -0,0 +1,18 @@
+"""Main method to enable command line execution"""
+
+import partitioner.single_runner as sr
+from partitioner.arguments import PartitionArguments
+
+if __name__ == "__main__":
+    args = PartitionArguments()
+    args.from_params(
+        catalog_name="td_demo",
+        input_path="/home/delucchi/td_data/wsource",
+        input_format="parquet",
+        ra_column="ra",
+        dec_column="decl",
+        id_column="diaObjectId",
+        pixel_threshold=1_000_000,
+        output_path="/home/delucchi/xmatch/catalogs/",
+    )
+    sr.run(args)
